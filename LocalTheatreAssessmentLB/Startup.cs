d6134ba3406b705ea43f2453925c84405d3200b9
luki110 <37291567+luki110@users.noreply.github.com>
2019-02-18 15:48:21 +0000

    Add project files.

---
+++ LocalTheatreAssessmentLB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalTheatreAssessmentLB.Startup))]
+namespace LocalTheatreAssessmentLB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
